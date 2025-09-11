0bb069b402281b7b01caade0fc6a781eb3e08cce
abangau <abangau@opentext.com>
2025-09-11 16:56:42 +0300

    customer file

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -29,58 +29,58 @@ public class MyStepdefs {
         // Double, Byte, Short, Long, BigInteger or BigDecimal.
         //
         // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
 
     @When("a payment method with bank account type {string}, bank account number & bank routing number is added using channel {string}")
     public void a_payment_method_with_bank_account_type_bank_account_number_bank_routing_number_is_added_using_channel(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @When("making a one-time payment with payment amount {string} and payment date {string} using channel {string}")
     public void making_a_one_time_payment_with_payment_amount_and_payment_date_using_channel(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("API response code should be {string} with payment confirmation number in response")
     public void api_response_code_should_be_with_payment_confirmation_number_in_response(String string) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("payment confirmation number should be added in DB with payment status scheduled")
     public void payment_confirmation_number_should_be_added_in_db_with_payment_status_scheduled() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("payment amount {string},payment date {string}, created by channel {string} should be added in DB")
     public void payment_amount_payment_date_created_by_channel_should_be_added_in_db(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("Memo is sent successfully when payment scheduled via digital channel")
     public void memo_is_sent_successfully_when_payment_scheduled_via_digital_channel() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("Delinquent Memo is sent successfully when payment scheduled via digital channel")
     public void delinquent_memo_is_sent_successfully_when_payment_scheduled_via_digital_channel() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("Email is published successfully when payment scheduled via digital channel")
     public void email_is_published_successfully_when_payment_scheduled_via_digital_channel() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("Open to buy should be updated")
     public void open_to_buy_should_be_updated() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @Then("Action entry should be updated")
     public void action_entry_should_be_updated() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
 
     @Given("an account of type {string},client {string} and delinquent between {string} {string} days with account conditions")
@@ -92,18 +92,18 @@ public class MyStepdefs {
         // Double, Byte, Short, Long, BigInteger or BigDecimal.
         //
         // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
 
     @When("a one-time payment with payment amount {string} and payment date {string}")
     public void a_one_time_payment_with_payment_amount_and_payment_date(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
     @When("making a one-time payment with bank account type {string}, bank account number & bank routing number in request using channel {string}")
     public void making_a_one_time_payment_with_bank_account_type_bank_account_number_bank_routing_number_in_request_using_channel(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
 
     @Given("an account of type {string} and client {string} with account conditions")
@@ -115,14 +115,27 @@ public class MyStepdefs {
         // Double, Byte, Short, Long, BigInteger or BigDecimal.
         //
         // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
 
     @When("a one-time payment with payment amount {string} and payment date {string} for settlement")
     public void a_one_time_payment_with_payment_amount_and_payment_date_for_settlement(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assert 1 == 1;
     }
+
+    @When("making a one-time payment with payment amount {string} and payment date {string} for settlement")
+    public void making_a_one_time_payment_with_payment_amount_and_payment_date_for_settlement(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        assert 1 == 1;
+    }
+
+    @Then("API response code should be {string} with error code and error message")
+    public void api_response_code_should_be_with_error_code_and_error_message(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        assert 1 == 1;
+    }
+
 
 
 }
