e4adfda431c93d6350678c352b086e5f6c02a7b8
abangau <abangau@opentext.com>
2025-09-11 16:49:47 +0300

    customer file

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -5,8 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 //@CucumberOptions(plugin = "com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results/ManualRunnerTest_OctaneGherkinResults.xml",
 //        features = "src/test/resources/F1/" , tags = "not @exclude")
-@CucumberOptions(features = "src/test/resources/F1",
-//        glue = {"MyStepdefs"},
+@CucumberOptions(features = "src/test/resources/F1/TestF.feature",
         plugin = { "junit:target/cucumber-reports.xml" },tags = "not @Excluded")
 
 public class MyTest {
